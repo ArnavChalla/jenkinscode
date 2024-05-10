8bc056f0da4627c66e0631816c5210da2a825372
Arnav Challa <137600072+ArnavChalla@users.noreply.github.com>
2024-05-10 22:52:30 +0530

    jenkins_python_code.py

--- jenkins_python_code.py
+++ jenkins_python_code.py
@@ -1,2 +1,2 @@
 print("Yo my name is Arnav Challa")
-#print("This message will be printed after two minutes(After Jenkins autobuilds)")
+print("This message will be printed after two minutes(After Jenkins autobuilds)")
